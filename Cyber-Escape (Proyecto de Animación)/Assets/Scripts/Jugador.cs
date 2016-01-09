b7a13040800d5eec35c49ce0401052f552be5cb8
Edward Fernández <edwardfernandez26@gmail.com>
2016-01-08 21:40:47 -0430

    Ya se cambian las animaciones al presionar botones.

---
+++ Cyber-Escape (Proyecto de Animación)/Assets/Scripts/Jugador.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jugador : MonoBehaviour {
+
+	// Variables públicas.
+	public Animator animacion;  // Manejador de las animaciones.
+
+	// Descripción:
+	// 		Función que me permite incializar variables.
+	void Start () {
+		animacion = GetComponent<Animator> (); 
+	}
+	
+	// Descripción:
+	// 		Función que se ejecutará cada frame.
+	void Update () {
+
+		
+		if ( Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) ) {
+			animacion.Play ("Corriendo", -1, 0f);
+
+		} else if (Input.GetKeyDown(KeyCode.Space) ){
+			animacion.Play ("Saltando", -1, 0f);
+		}
+	}
+}
