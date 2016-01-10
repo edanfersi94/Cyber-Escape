119e314c8a5ed6241f3bfe2c07c0c6dda66b0d08
Edward Fernández <edwardfernandez26@gmail.com>
2016-01-10 14:23:25 -0430

    Aparecen aleatoriamente los enemigos.

--- Cyber-Escape (Proyecto de Animación)/Assets/Scripts/Generador.cs
+++ Cyber-Escape (Proyecto de Animación)/Assets/Scripts/Generador.cs
@@ -7,17 +7,22 @@ public class Generador: MonoBehaviour {
 	public GameObject[] listaObjetos; 	          // Objetos a instanciar.
 	public GameObject objetoLimite;               // Objeto que indicará hasta aparecerán los objetos a instanciar.
 	public float[] carriles = {-1.1f, 0f, 1.1f};  // Ubicación de los carriles.
+	// Variable que permitirá modificar el angulo del objeto a instanciar.
+	public int anguloX;
 
 	// Variables para el manejo del tiempo entre cada instanciación.
 	public float tiempoMin = 50f;
 	public float tiempoMax = 300f;
-
-	// Variable que permitirá modificar el angulo del objeto a instanciar.
-	public int anguloX;
+	private int ultimaPosicion;
+	private int objetoElegido;
+	private int ultimoObjeto;
 
 	// Descripción:
 	// 		Función que me permite incializar variables.
 	void Start () {
+		objetoElegido = Random.Range (0, listaObjetos.Length);
+		ultimoObjeto = objetoElegido;
+		ultimaPosicion = -1;
 		Generar ();
 	}
 
@@ -28,16 +33,28 @@ public class Generador: MonoBehaviour {
 	// Descripción:
 	// 		Función que permite generar (instanciar) los objetos seleccionados.
 	public void Generar(){
-		float posicionRandomZ = Random.Range (0, 50);
-		float posicionRandomX = Random.Range (0, 3);
-		print (carriles[(int) posicionRandomX]);
+		float posicionRandomZ = Random.Range (0, 30);
+		int posicionRandomX = Random.Range (0, 3);
 
+		if (objetoElegido == 1 && ultimoObjeto != 1) {
+			ultimaPosicion = 1;
+		} else {
+			if (ultimaPosicion != posicionRandomX) {
+				ultimaPosicion = posicionRandomX;
+			} else {
+				Generar ();
+			}
+		}
+	
+	
 		print ("Hola");
 		// Nueva posición donde se hará la instanciación.
-		transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + posicionRandomZ);
+		transform.position = new Vector3 (carriles[ultimaPosicion], transform.position.y, transform.position.z + posicionRandomZ);
 
 		if (transform.position.z < objetoLimite.gameObject.transform.position.z) {
-			Instantiate (listaObjetos [Random.Range (0, listaObjetos.Length)], transform.position, Quaternion.AngleAxis(anguloX, new Vector3(1, 0, 0)));
+			Instantiate (listaObjetos [objetoElegido], transform.position, Quaternion.AngleAxis(anguloX, new Vector3(1, 0, 0)));
+			ultimoObjeto = objetoElegido;
+			objetoElegido = Random.Range (0, listaObjetos.Length);
 			Invoke ("Generar", Random.Range (tiempoMin, tiempoMax));
 		} else {
 			Destroy (this.gameObject);
