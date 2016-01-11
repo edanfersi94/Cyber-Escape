d2be2e6f5d2da362f0d9f89ccd85bcd2c6f39713
oggs22 <oggs_22@hotmail.com>
2016-01-10 23:07:11 -0430

    Agarra tipografia

--- Cyber-Escape (Proyecto de Animación)/Assets/Scripts/DestruirMoneda.cs
+++ Cyber-Escape (Proyecto de Animación)/Assets/Scripts/DestruirMoneda.cs
@@ -3,11 +3,15 @@ using System.Collections;
 
 public class DestruirMoneda : MonoBehaviour {
 	public int Coin=0;
+
+
+
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
@@ -16,6 +20,8 @@ public class DestruirMoneda : MonoBehaviour {
 	void OnCollisionEnter(Collision objeto){
 		
 		if (objeto.gameObject.tag == "Moneda") {
+
+
 			
 			Destroy (objeto.gameObject);
 			Coin += 1;
@@ -26,6 +32,15 @@ public class DestruirMoneda : MonoBehaviour {
 
 	void OnGUI()
 	{
-		GUILayout.Label( "Score = " + Coin);
+		GUIStyle myStyle = new GUIStyle ();
+		myStyle.fontSize = 50;
+
+		Font myFont = (Font)Resources.Load("Fonts/HACKED", typeof(Font));
+		myStyle.font = myFont;
+
+		//myStyle.normal.textColor = Color.red;
+		//myStyle.hover.textColor = Color.red;
+		
+	 	GUI.Label(new Rect(10,10, 100, 30), "Score = " + Coin, myStyle);
 	}
 }
