ae772fe20f5f047be3c6a2f4a8ce003e49f6aeda
Edward Fernández <edwardfernandez26@gmail.com>
2016-01-14 20:08:31 -0430

    nueva escena.

---
+++ Cyber-Escape (Proyecto de Animación)/Assets/gameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameController : MonoBehaviour {
+
+	public GUIText gameOverText;
+
+	private bool gameOver;
+
+	void Start () {
+		gameOver = false;
+		gameOverText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GameOver(){
+		gameOverText.text = "Game Over";
+		gameOver = true;
+	}
+}
